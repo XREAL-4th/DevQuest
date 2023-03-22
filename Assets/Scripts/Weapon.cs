827c2465121e68b3c6cd381596936c4bc56ab248
Hyunmin Cho <chm49@naver.com>
2023-03-22 22:56:40 +0900

    [Feat] 2번 과제 구현
    2번 과제인 마우스 클릭으로 발사를 구현하였고 3번 과제인 커서 위치로 발사에서 원하는대로 동작하지 않습니다.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,7 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Weapon : MonoBehaviour
 {
+    public GameObject bulletPosition;
+    public GameObject prefabBullet;
+    float bulletSpeed = 5;
+    Vector3 direction;
     public int damage = 20;
+    public Camera cam;
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        Shot();
+        direction = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.transform.position.z));
+    }
+    void Shot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(prefabBullet);
+            bullet.transform.position = bulletPosition.transform.position;
+            bullet.gameObject.GetComponent<Rigidbody>().AddForce(direction * bulletSpeed, ForceMode.Impulse);
+            //Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit rayHit;
+            //if (Physics.Raycast(ray, out rayHit, 100))
+            //{
+            //    Vector3 nextVector = rayHit.point - transform.position;
+            //    nextVector.y = 0;
+            //    transform.LookAt(transform.position + nextVector);
+            //}
+        }
+    }
 }
+
+
