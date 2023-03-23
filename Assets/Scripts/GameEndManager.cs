efa375a8d82e97a6757498bb93cd8144b69ded11
Sharlotte <seeseafood@naver.com>
2023-03-24 05:19:35 +0900

    feat: gameEndManager 추가

---
+++ Assets/Scripts/GameEndManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class GameEndManager : DDOLSingletonMonoBehaviour<GameEndManager>
+{
+    public bool isGameEnd = false;
+}
