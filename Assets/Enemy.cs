23181ef335f05d3e4e3fc6df723f15089d1818a6
kkkkiiiiii <alohomoratolit@naver.com>
2023-03-29 14:15:00 +0900

    feat : state 추가
    ChasingPlayer State
    ChasingRedBall State 추가
    
    감지 거리 Redball>player>Attack Range

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -13,12 +13,16 @@ public class Enemy : MonoBehaviour
     
     [Header("Settings")]
     [SerializeField] private float attackRange;
+    [SerializeField] private float chasingRedBallRange;
+    [SerializeField] private float chasingPlayerRange;
     
     public enum State 
     {
         None,
         Idle,
-        Attack
+        Attack,
+        ChasingRedBall,
+        ChasingPlayer
     }
     
     [Header("Debug")]
@@ -41,11 +45,7 @@ public class Enemy : MonoBehaviour
             switch (state) 
             {
                 case State.Idle:
-                    //1 << 6인 이유는 Player의 Layer가 6이기 때문
-                    if (Physics.CheckSphere(transform.position, attackRange, 1 << 6, QueryTriggerInteraction.Ignore))
-                    {
-                        nextState = State.Attack;
-                    }
+                    CheckArea();
                     break;
                 case State.Attack:
                     if (attackDone)
@@ -53,6 +53,12 @@ public class Enemy : MonoBehaviour
                         nextState = State.Idle;
                         attackDone = false;
                     }
+                    break;
+                case State.ChasingPlayer:
+                    CheckArea();
+                    break;
+                case State.ChasingRedBall:
+                    CheckArea();
                     break;
                 //insert code here...
             }
@@ -70,6 +76,12 @@ public class Enemy : MonoBehaviour
                 case State.Attack:
                     Attack();
                     break;
+                case State.ChasingPlayer:
+                    ChasingPlayer();
+                    break;
+                case State.ChasingRedBall:
+                    ChasingRedBall();
+                    break;
                 //insert code here...
             }
         }
@@ -77,10 +89,35 @@ public class Enemy : MonoBehaviour
         //3. 글로벌 & 스테이트 업데이트
         //insert code here...
     }
-    
+
+    private void CheckArea()
+    {
+        //1 << 6인 이유는 Player의 Layer가 6이기 때문
+        if (Physics.CheckSphere(transform.position, attackRange, 1 << 6, QueryTriggerInteraction.Ignore))
+        {
+            nextState = State.Attack;
+        }else if (Physics.CheckSphere(transform.position,chasingPlayerRange,1<<6,QueryTriggerInteraction.Ignore))
+        {
+            // Chasing Player
+            nextState = State.ChasingPlayer;
+        }else if (Physics.CheckSphere(transform.position,chasingRedBallRange,1<<9,QueryTriggerInteraction.Ignore))
+        {
+            // Chasing RedBall
+            nextState = State.ChasingRedBall;
+        }
+    }
     private void Attack() //현재 공격은 애니메이션만 작동합니다.
     {
         animator.SetTrigger("attack");
+    }
+
+    private void ChasingPlayer()
+    {
+        Debug.Log("chasingPlayer");
+    }
+    private void ChasingRedBall()
+    {
+        Debug.Log("chasingRedBall");
     }
 
     public void InstantiateFx() //Unity Animation Event 에서 실행됩니다.
@@ -100,5 +137,9 @@ public class Enemy : MonoBehaviour
         //해당 함수는 없어도 기능 상의 문제는 없지만, 기능 체크 및 디버깅을 용이하게 합니다.
         Gizmos.color = new Color(1f, 0f, 0f, 0.5f);
         Gizmos.DrawSphere(transform.position, attackRange);
+        Gizmos.color = new Color(0f, 1f, 0f, 0.2f);
+        Gizmos.DrawSphere(transform.position, chasingPlayerRange);
+        Gizmos.color = new Color(0f, 0f, 1f, 0.1f);
+        Gizmos.DrawSphere(transform.position, chasingRedBallRange);
     }
 }
