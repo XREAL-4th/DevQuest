534a510a5c4c3faf51ca282a402b866ad12dde3c
yoshii <113421649+yooksj13@users.noreply.github.com>
2023-03-29 20:56:11 +0900

    스킬1 구현
    스킬1 구현
    뭔가 애쉬 스킬 같은..

---
+++ Assets/Scripts/PlayerSkills.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkills : MonoBehaviour
+{
+    
+	IEnumerator coroutine;
+
+	public GameObject weaponPrefab;
+	void Update()
+	{
+        if (Input.GetKey(KeyCode.E))
+        {
+			coroutine = Slice();
+			StartCoroutine(coroutine);
+
+		}
+	}
+
+	IEnumerator Slice()
+	{
+		yield return StartCoroutine(Shoot(1f));
+		yield return StartCoroutine(Shoot(2f));
+		yield return StartCoroutine(Shoot(3f));
+		yield return StartCoroutine(Shoot(4f));
+		yield return StartCoroutine(Shoot(5f));
+		yield return StartCoroutine(Shoot(6f));
+		yield return StartCoroutine(Shoot(7f));
+		yield return StartCoroutine(Shoot(8f));
+		yield return StartCoroutine(Shoot(9f));
+	}
+
+	IEnumerator Shoot(float num)
+    {
+		GameObject weapon = Instantiate(weaponPrefab, transform.position, transform.rotation) as GameObject;
+		Ray ray = Camera.main.ScreenPointToRay(new Vector3((Camera.main.pixelWidth * num) / 10, Camera.main.pixelHeight / 2));
+		Vector3 shooting = ray.direction;
+		shooting = shooting.normalized * 4000;
+		weapon.GetComponent<WeaponController>().Launch(shooting);
+		yield break;
+	}
+}
