66b266e8680fc1e90c05b007afb4543a46fe5662
Sharlotte <seeseafood@naver.com>
2023-03-30 18:31:17 +0900

    feat: 구조분해 테스트용 구조분해 유틸 클래스 추가

---
+++ Assets/Scripts/Utils/Overrides/Deconstructs.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class Deconstructs
+{
+    public static void Deconstruct(this Vector3 vec3, out float x, out float y, out float z)
+    {
+        x = vec3.x; y = vec3.y; z = vec3.z;
+    }
+}
