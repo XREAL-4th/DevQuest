6f38b79236c8a5d2a6ccab035543014ef76e2e7a
hyeonmin11 <79415795+hyeonmin11@users.noreply.github.com>
2023-04-01 00:48:35 +0900

    player move제대로 하게, bullet 어디든지 닿으면 사라지게 수정

--- Assets/Scripts/MonoBehavior/Bullet.cs
+++ Assets/Scripts/MonoBehavior/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public GameObject hitfx_bullet;
     public GameObject hitfx_head;
     public GameObject hitfx_body;
     // Update is called once per frame
@@ -11,20 +12,23 @@ public class Bullet : MonoBehaviour
     {
         //프레임마다 오브젝트를 로컬좌표상에서 앞으로 1의힘으로 날아간다
         transform.Translate(Vector3.forward * 0.1f);
+        Instantiate(hitfx_bullet, transform.position, Quaternion.identity);
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
+        gameObject.SetActive(false);
         if(collision.collider.gameObject.CompareTag("Enemy_head")){
             
-            gameObject.SetActive(false);
+            
             Instantiate(hitfx_head, collision.transform.position, Quaternion.identity);
+            
         }
         
         if(collision.collider.gameObject.CompareTag("Enemy_Body")){
             
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
             Instantiate(hitfx_body, collision.transform.position, Quaternion.identity);
         }
         
