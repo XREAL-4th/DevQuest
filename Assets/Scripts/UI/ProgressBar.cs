a43d80a33bd0f8daa6f5107ea2a0cce9a756268e
Sharlotte <seeseafood@naver.com>
2023-03-31 19:47:26 +0900

    feat: progress bar 변화 쫀득하게 개선

--- Assets/Scripts/UI/ProgressBar.cs
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements.Experimental;
 
 public class ProgressBar : MonoBehaviour
 {
@@ -21,14 +22,14 @@ public class ProgressBar : MonoBehaviour
     {
         frontImage.color = color;
         Color.RGBToHSV(color, out float H, out float S, out float V);
-        shadowImage.color = Color.HSVToRGB(H, S * 0.8f, V);
-        backImage.color = new Color(11, 11, 11);
+        shadowImage.color = Color.HSVToRGB(H, S * 0.5f, V);
+        backImage.color = new Color(125, 125, 125);
     }
 
     private void Update()
     {
-        frontImage.rectTransform.SetLeft(-progress * rect.sizeDelta.x);
-
-        Timer.Main.SetTimeout(1, () => shadowImage.rectTransform.SetLeft(-progress * rect.sizeDelta.x));
+       
+        frontImage.fillAmount = Mathf.Lerp(frontImage.fillAmount, progress, Time.deltaTime * 5);
+        shadowImage.fillAmount = Mathf.Lerp(shadowImage.fillAmount, progress, Time.deltaTime * 2);
     }
 }
