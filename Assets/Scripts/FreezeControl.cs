341547e053cdce9304d39ebf64679ef30d0a5f19
Serin Yoon <bcode429@gmail.com>
2023-04-02 18:40:11 +0900

    upload 3/30 assignment

--- Assets/Scripts/FreezeControl.cs
+++ Assets/Scripts/FreezeControl.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FreezeControl : MonoBehaviour
 {
-    void Update()
+    private GameObject fill;
+    private float time = 4f;
+    
+    private void Start() {
+        fill = GameObject.Find("Fill");
+    }
+
+    private void Update()
     {
         Collider[] hit = Physics.OverlapSphere(transform.position, 6.0f);
         foreach (Collider c in hit) {
             if (c.name.Substring(0, 5) == "Enemy") {
                 Destroy(c.gameObject);
+                Destroy(c.gameObject.transform.GetChild(4));
                 GameManager.instance.score += 3;
             }
         }
+
+        time -= Time.deltaTime;
+        var per = time / 4f;
+        fill.GetComponent<Image>().fillAmount = per;
+
+        //fill.fillAmount = per;
     }
 }
---
+++ Assets/Scripts/QuitControl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class QuitControl : MonoBehaviour
+{
+    public GameObject popup;
+    public GameObject camera;
+
+    private void Start() {
+        popup.SetActive(false);
+    }
+
+    public void OnClickQuit() {
+        popup.SetActive(true);
+        Debug.Log("Quit clicked");
+        camera.GetComponent<CameraControl>().enabled = false;
+        camera.GetComponent<RayControl>().enabled = false;
+    }
+
+    public void OnClickYes() {
+        Debug.Log("Yes clicked");
+        SceneManager.LoadScene("Assignment");
+    }
+
+    public void OnClickNo() {
+        Debug.Log("No clicked");
+        popup.SetActive(false);
+        camera.GetComponent<CameraControl>().enabled = true;
+        camera.GetComponent<RayControl>().enabled = true;
+    }
+}
