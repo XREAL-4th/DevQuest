3500c7bb10783a791eefd7b79b68897a091d0546
kgy6022 <kgy6022@naver.com>
2023-04-03 21:03:53 +0900

    [fix] UI

--- Assets/Scripts/GameQuit.cs
+++ Assets/Scripts/GameQuit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameOut : MonoBehaviour
+public class GameQuit : MonoBehaviour
 {
     [SerializeField] private Button btnQuit, btnYes, btnNo;
     [SerializeField] private GameObject askPanel;
