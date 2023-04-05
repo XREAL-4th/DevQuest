b7f1fd49ac23b365530947813ceff6cefe87e813
chaeyeonS2 <321codus@naver.com>
2023-04-05 20:53:24 +0900

    총알 발사 구현

--- Assets/Scripts/VR/shoot.cs
+++ Assets/Scripts/VR/shoot.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class shoot : MonoBehaviour
 {
-    private OVRGrabbable ovrGrabbable;
-    public OVRInput.Button shootingButton;
+    [SerializeField] private OVRGrabbable ovrGrabbable;
+    //public OVRInput.Button shootingButton;
 
     public GameObject firePos;  //ÃÑ¾Ë »ý¼º À§Ä¡
     public GameObject PrefabBullet;
     //ÃÑ¾Ë ¼Óµµ
-    private float force = 5;
+    private float force = 2500;
     public int damage = 5;
 
     Vector3 dir;
@@ -18,14 +18,15 @@ public class shoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ovrGrabbable = OVRInput.Axis1D.PrimaryIndexTrigger();
+        //ovrGrabbable = gameObject.GetComponent<distance>
+        //OVRInput.Axis1D.PrimaryIndexTrigger();
     }
 
     // Update is called once per frame
     void Update()
     {
         //trigger ´©¸¦ ¶§
-        if (ovrGrabbable.isGrabbed && OVRInput.GetDown(shootingButton, ovrGrabbable.grabbedBy.GetController()))
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
         {
             TriggerShoot();
         }
@@ -35,6 +36,7 @@ public class shoot : MonoBehaviour
         GameObject bullet = Instantiate(PrefabBullet);    //ÇÁ¸®ÆÕ ÃÑ¾Ë »ý¼º
         bullet.GetComponent<Bullet>().damage = this.damage;
         bullet.transform.position = firePos.transform.position;
+        bullet.GetComponent<Rigidbody>().AddForce(Vector3.forward * force);
         //bullet.GetComponent<Rigidbody>().AddForce(dir * force);
 
     }
