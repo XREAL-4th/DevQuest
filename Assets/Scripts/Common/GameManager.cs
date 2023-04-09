1df6f067257eedbe9ce53586d04f71897f82c039
MINHA-J <harororo721@khu.ac.kr>
2023-04-09 21:55:25 +0900

    [Save] 임시

--- Assets/Scripts/Common/GameManager.cs
+++ Assets/Scripts/Common/GameManager.cs
@@ -98,10 +98,11 @@ public class GameManager : MonoBehaviour
             GameObject UIPrefab = Resources.Load<GameObject>("Prefabs/ExitUI");
             //GameObject UI = Instantiate(UIPrefab, new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2), Quaternion.identity, GameObject.Find("Canvas").transform);
             GameObject UI = Instantiate(UIPrefab,
-                new Vector3(GameObject.Find("Canvas").transform.position.x, GameObject.Find("Canvas").transform.position.y + 3.0f, 0),
+                new Vector3(GameObject.Find("Canvas").transform.position.x, GameObject.Find("Canvas").transform.position.y + 3.0f, GameObject.Find("Canvas").transform.position.z),
                 Quaternion.identity,
                 GameObject.Find("Canvas").transform);
         }
+
     }
 
     public void RestartGame()
